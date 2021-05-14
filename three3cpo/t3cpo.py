50e164bd7d8a723d3e600f6122cdd37d1d10a22e
deferment <deferment@gmail.com>
2021-05-14 14:53:35 +0200

    added marketplace api wrapper

--- three3cpo/t3cpo.py
+++ three3cpo/t3cpo.py
@@ -1,11 +1,15 @@
 from wrapper.bots import BotsApi
 from wrapper.accounts import AccountsApi
 from wrapper.deals import DealsApi
+from wrapper.grid_bots import GridBotsApi
+from wrapper.marketplace import MarketPlaceApi
 
 
 class Three3Cpo(object):
 
     def __init__(self, key, secret):
-        self.bots = BotsApi(key=key, secret=secret)
-        self.accounts = AccountsApi(key=key, secret=secret)
-        self.deals = DealsApi(key=key, secret=secret)
+        self.Bots = BotsApi(key=key, secret=secret)
+        self.Accounts = AccountsApi(key=key, secret=secret)
+        self.Deals = DealsApi(key=key, secret=secret)
+        self.GridBots = GridBotsApi(key=key, secret=secret)
+        self.MarketPlace = MarketPlaceApi(key=key, secret=secret)
---
+++ three3cpo/wrapper/marketplace.py
@@ -0,0 +1,14 @@
+from wrapper.api import Api
+
+
+class MarketPlaceApi(Api):
+
+    def __init__(self, key, secret):
+        super().__init__(key, secret)
+        self.relative_base = '/public/api/ver1/marketplace'
+
+    def list_marketplace_items(self, params=None):
+        return self.invoke('GET', f'{self.relative_base}/items', params=params).json()
+
+    def get_item_signal(self, item_id: int, params=None):
+        return self.invoke('GET', f'{self.relative_base}/{item_id}/signals', params=params).json()
