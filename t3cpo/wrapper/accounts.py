d964785bc355a03705c9536edd03df36d3a1f355
mMeijden <deferment@gmail.com>
2021-05-15 13:59:20 +0200

    completed accounts wrapper

--- t3cpo/wrapper/accounts.py
+++ t3cpo/wrapper/accounts.py
@@ -6,66 +6,71 @@ class AccountsApi(Api):
     def __init__(self, key, secret):
         super().__init__(key, secret)
 
-    def list(self):
+    def get_accounts(self):
         return self.invoke('GET', '/public/api/ver1/accounts').json()
 
-    # POST /ver1/accounts/transfer
-    def transfer(self, currency: str, amount: float, to_account_id: int, from_account_id: int):
-        payload = {
-            'currency': currency,
-            'amount': amount,
-            'to_account_id': to_account_id,
-            'from_account_id': from_account_id
-        }
-        return self.invoke('POST', '/public/api/ver1/accounts/transfer', params=payload).json()
+    def transfer(self, params: dict):
+        return self.invoke('POST', '/public/api/ver1/accounts/transfer', params=params).json()
 
-    # GET /ver1/accounts/transfer_history
-    def transfer_history(self, account_id: int, currency: str, page: int = 1, per_page: int = 10):
-        payload = {
-            'account_id': account_id,
-            'currency': currency,
-            'page': page,
-            'per_page': per_page
-        }
-        return self.invoke('GET', '/public/api/ver1/accounts/transfer_history', params=payload).json()
+    def transfer_history(self, params: dict):
+        return self.invoke('GET', '/public/api/ver1/accounts/transfer_history', params=params).json()
 
-    # GET /ver1/accounts/transfer_data
     def transfer_data(self):
         return self.invoke('GET', '/public/api/ver1/accounts/transfer_data').json()
 
-    # POST /ver1/accounts/new
-    def new_account(self, account_type: str, name: str, **kwargs):
-        payload = {
-            'type': account_type,
-            'name': name,
-            **kwargs
-        }
-        return self.invoke('POST', '/public/api/ver1/accounts/new', params=payload).json()
+    def new_account(self, params: dict):
+        return self.invoke('POST', '/public/api/ver1/accounts/new', params=params).json()
 
-    # POST /ver1/accounts/update
-    def edit_account(self, account_id: int, **kwargs):
-        payload = {
-            'account_id': account_id,
-            **kwargs
-        }
-        return self.invoke('POST', '/public/api/ver1/accounts/new', params=payload).json()
+    def edit_account(self, params: dict):
+        return self.invoke('POST', '/public/api/ver1/accounts/update', params=params).json()
 
-    # GET /ver1/accounts/market_pairs
-    def market_pairs(self):
-        pass
+    def market_list(self):
+        return self.invoke('GET', '/public/api/ver1/accounts/market_list').json()
 
-    # GET /ver1/accounts/currency_rates
-    # GET /ver1/accounts/{account_id}/deposit_data
-    # GET /ver1/accounts/{account_id}/networks_info
-    # POST /ver1/accounts/{account_id}/convert_dust_to_bnb
-    # GET /ver1/accounts/{account_id}/active_trading_entities
-    # POST /ver1/accounts/{account_id}/sell_all_to_usd
-    # POST /ver1/accounts/{account_id}/sell_all_to_btc
-    # GET /ver1/accounts/{account_id}/balance_chart_data
-    # POST /ver1/accounts/{account_id}/load_balances
-    # POST /ver1/accounts/{account_id}/rename
-    # POST /ver1/accounts/{account_id}/pie_chart_data
-    # POST /ver1/accounts/{account_id}/account_table_data
-    # POST /ver1/accounts/{account_id}/remove
-    # GET /ver1/accounts/{account_id}/leverage_data
-    # GET /ver1/accounts/{account_id}
+    def market_pairs(self, params: dict):
+        return self.invoke('GET', '/public/api/ver1/accounts/market_pairs', params).json()
+
+    def currency_rates(self, params: dict):
+        return self.invoke('GET', '/public/api/ver1/accounts/currency_rates', params).json()
+
+    def deposit_data(self, account_id: int, params: dict):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}/deposit_data', params).json()
+
+    def networks_info(self, account_id: int, params: dict = None):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}/networks_info', params).json()
+
+    def convert_dust_to_bnb(self, account_id: int, params: dict = None):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/convert_dust_to_bnb', params).json()
+
+    def active_trading_entities(self, account_id: int):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}/active_trading_entities').json()
+
+    def sell_all_to_usd(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/sell_all_to_usd').json()
+
+    def sell_all_to_btc(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/sell_all_to_btc').json()
+
+    def balance_chart_data(self, account_id: int, params):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}/balance_chart_data', params).json()
+
+    def load_balances(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/load_balances').json()
+
+    def rename_exchange_connection(self, account_id: int, params):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/rename', params).json()
+
+    def pie_chart_data(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/pie_chart_data').json()
+
+    def account_table_data(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/account_table_data').json()
+
+    def remove_exchange_connection(self, account_id: int):
+        return self.invoke('POST', f'/public/api/ver1/accounts/{account_id}/remove').json()
+
+    def account_leverage_data(self, account_id: int):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}/leverage_data').json()
+
+    def account_info(self, account_id: int):
+        return self.invoke('GET', f'/public/api/ver1/accounts/{account_id}').json()
