8bbcddc467e48136c41cce66ef4115203f4f3b58
mMeijden <deferment@gmail.com>
2021-11-01 22:45:50 +0100

    added additional functions to switch between trading modes, added new get current mode api option

--- t3cpo/wrapper/users.py
+++ t3cpo/wrapper/users.py
@@ -7,5 +7,14 @@ class UsersApi(Api):
         super().__init__(key, secret)
         self.relative_base = '/public/api/ver1/users'
 
+    def get_current_mode(self):
+        return self.invoke('GET', f'{self.relative_base}/current_mode').json()
+
     def change_mode(self, params):
         return self.invoke('POST', f'{self.relative_base}/change_mode', params=params).json()
+
+    def change_to_real_trading(self):
+        return self.invoke('POST', f'{self.relative_base}/change_mode', params={'mode': 'real'}).json()
+
+    def change_to_paper_trading(self):
+        return self.invoke('POST', f'{self.relative_base}/change_mode', params={'mode': 'paper'}).json()
